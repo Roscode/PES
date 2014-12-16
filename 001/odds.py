eec2341dce6a1e4b1a310b7a0382ee9c72c3c56f
Clint Valentine <valentine.clint@gmail.com>
2014-12-16 00:54:01 -0500

    Golfed the script a bit

--- 001/odds.py
+++ 001/odds.py
@@ -11,10 +11,10 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 from __future__ import print_function
 
-VALID = []
+ANSWER = 0
 
 for x in xrange(1000):
     if x != 0 and (x % 3 == 0 or x % 5 == 0):
-        VALID.append(x)
+        ANSWER += x
 
-print(sum(VALID))
+print(ANSWER)
