19088f6c042e65de6a05da65b9c68cf0c09f755e
Clint Valentine <valentine.clint@gmail.com>
2014-12-16 00:45:06 -0500

    Added first problem folder and Python start

---
+++ 001/odds.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+#Copyright 2014 Clint Valentine & Darren Roscoe
+
+"""
+If we list all the natural numbers below 10 that are multiples of
+3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+from __future__ import print_function
+
+def is_three_five()
+for x in xrange(10):
+    return x if x % 3 is 0
